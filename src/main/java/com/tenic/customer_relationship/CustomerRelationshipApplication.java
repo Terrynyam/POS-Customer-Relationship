d0883a501b023c1bd98f68cbdb245d4bf788f424
terrancen <terrancen@afrosoft.co.zw>
2024-06-05 18:20:04 +0200

    initial commit

---
+++ src/main/java/com/tenic/customer_relationship/CustomerRelationshipApplication.java
@@ -0,0 +1,13 @@
+package com.tenic.customer_relationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRelationshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerRelationshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenic/customer_relationship/CustomerRelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenic.customer_relationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRelationshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
